31c6209a6803b21f80f23a39fb2967f17459c942
Frank Assumma <fassumma@calpoly.edu>
2023-10-02 11:37:52 -0700

    Added query for name

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -12,3 +12,50 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}');
 });
+
+const users = { 
+    users_list :
+    [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
+
+ app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+ });
+
+ const findUserByName = (name) => {
+    return users['users_list'].filter( (user) => user['name'] == name);
+ }
