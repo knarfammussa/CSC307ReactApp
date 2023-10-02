4dd812c9ddad63210554d26d38a4cabe98756b9e
Frank <fassumma@calpoly.edu>
2023-10-02 00:57:49 -0700

    Remove added

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -6,6 +6,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -15,9 +16,15 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+	        <td>{row.name}</td>
+	        <td>{row.job}</td>
+	        <td>
+			    <button onClick={() => 
+				    props.removeCharacter(index)}>
+				    Delete
+			    </button>
+	        </td>
+	      </tr>
       );
      }
     );
@@ -28,13 +35,14 @@ function TableBody(props) {
      );
   }
 
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} 
+        removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
